d039a9ba9c3e32731c4c76d7d3995c7e672ebd62
Olozavr <olkovko64@gmail.com>
2024-12-14 16:34:37 +0300

    начало

---
+++ Itog1/app/build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
+}
+
+detekt {
+    config = files("detekt.yml")
+    buildUponDefaultConfig = true
+    allRules = false
+}
+
+android {
+    namespace = "com.example.itog1"
+    compileSdk = 34
+    packagingOptions {
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/INDEX.LIST")
+        exclude("META-INF/LICENSE")
+        exclude("META-INF/LICENSE.txt")
+        exclude("META-INF/NOTICE")
+        exclude("git.properties")
+    }
+    defaultConfig {
+        applicationId = "com.example.itog1"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+
+dependencies {
+
+    // Основные библиотеки
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.dataframe)
+    testImplementation(libs.junit)
+    testImplementation(libs.commonsLang)
+    testImplementation(libs.commons.io.v2130)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.core)
+    androidTestImplementation(libs.kakao)
+
+
+    implementation(libs.commons.math3)
+}
---
+++ Itog1/app/src/main/java/com/example/itog1/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.itog1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.exp
+import kotlin.math.sqrt
+import java.util.Random
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val meanEditText = findViewById<EditText>(R.id.mean_val)
+        val varianceEditText = findViewById<EditText>(R.id.variance_value)
+        val generateButton = findViewById<Button>(R.id.get_random_num)
+        val resultTextView = findViewById<TextView>(R.id.random_number_result)
+
+
+        generateButton.setOnClickListener {
+            try {
+                val meanText = meanEditText.text.toString()
+                val varianceText = varianceEditText.text.toString()
+
+
+                if (meanText.isEmpty() || varianceText.isEmpty()) {
+                    resultTextView.text = "Пожалуйста, заполните оба поля."
+                    return@setOnClickListener
+                }
+
+                val mean = meanText.toDoubleOrNull()
+                val variance = varianceText.toDoubleOrNull()
+
+                if (mean == null || variance == null || variance < 0) {
+                    resultTextView.text = "Ошибка: некорректные значения µ или σ²."
+                    return@setOnClickListener
+                }
+
+
+                val randomValue = generateLogNormal(mean, sqrt(variance))
+                resultTextView.text = "$randomValue"
+
+
+                lastGeneratedNumber = resultTextView.text.toString()
+
+            } catch (e: Exception) {
+                resultTextView.text = "Произошла ошибка: ${e.message}"
+            }
+        }
+    }
+
+    private fun generateLogNormal(mean: Double, stdDev: Double): Double {
+
+        val normalValue = Random().nextGaussian() * stdDev + mean
+        return exp(normalValue)
+    }
+
+    private var lastGeneratedNumber: String? = null
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("LAST_NUMBER", lastGeneratedNumber)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        lastGeneratedNumber = savedInstanceState.getString("LAST_NUMBER")
+        val resultTextView = findViewById<TextView>(R.id.random_number_result)
+        resultTextView.text = lastGeneratedNumber ?: "Результат будет здесь"
+    }
+}
